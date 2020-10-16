260cf549c76f63efce04f3a747415b3ee1403e70
Ann Catton <ann.catton@oicr.on.ca>
2020-10-16 18:24:00 -0400

    add logs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 const withCSS = require('@zeit/next-css');
+
+if (process.env.DEBUG) {
+  console.log('EGO_API_ROOT: ', process.env.NEXT_PUBLIC_EGO_API_ROOT);
+  console.log('EGO_CLIENT_ID: ', process.env.NEXT_PUBLIC_EGO_CLIENT_ID);
+  console.log('ARRANGER_PROJECT_ID: ', process.env.NEXT_PUBLIC_ARRANGER_PROJECT_ID);
+  console.log('ARRANGER_GRAPHQL_FIELD: ', process.env.NEXT_PUBLIC_ARRANGER_GRAPHQL_FIELD);
+  console.log('ARRANGER_INDEX: ', process.env.NEXT_PUBLIC_ARRANGER_INDEX);
+  console.log('ARRANGER_API: ', process.env.NEXT_PUBLIC_ARRANGER_API_URL);
+}
+
 module.exports = withCSS({
   publicRuntimeConfig: {
     EGO_API_ROOT: process.env.NEXT_PUBLIC_EGO_API_ROOT,
