01523162b7e53ca93f08e19a84f12c55a05b40f3
Anders Richardsson <2107110+caravinci@users.noreply.github.com>
2022-03-30 15:12:58 -0400

    update for Arranger aggregations components - round 2

--- components/pages/explorer/RepoTable.tsx
+++ components/pages/explorer/RepoTable.tsx
@@ -20,7 +20,7 @@
  */
 
 import { css, useTheme } from '@emotion/react';
-import { Table } from '@overture-stack/arranger-components';
+import { Table, useArrangerTheme } from '@overture-stack/arranger-components';
 import urlJoin from 'url-join';
 
 import { PageContentProps } from './index';
@@ -65,14 +65,6 @@ const getTableStyle = (theme: DMSThemeInterface) => css`
       }
       & .dropDownHeader button {
         margin-right: 8px;
-      }
-      & .dropDownHeader button:after {
-        content: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23151c3d' fill-rule='evenodd' d='M9.952 3.342c.468-.456 1.228-.456 1.697 0 .234.228.351.526.351.825 0 .298-.117.597-.351.825l-4.8 4.666c-.469.456-1.23.456-1.697 0l-4.8-4.666c-.47-.456-.47-1.194 0-1.65.468-.456 1.228-.456 1.696 0L6 7.184l3.952-3.842z'/%3E%3C/svg%3E");
-        margin-top: 2px;
-        margin-left: -3px;
-      }
-      & .dropDownButton svg {
-        display: none;
       }
       & div.dropDownContent {
         right: 7px !important;
@@ -223,8 +215,20 @@ const getTableStyle = (theme: DMSThemeInterface) => css`
   }
 `;
 
+const getTableStyles = (theme: DefaultTheme) => ({
+  components: {
+    Table: {
+      DropDown: {
+        arrowColor: '#151c3d',
+        arrowTransition: 'all 0s',
+      },
+    },
+  },
+});
+
 const RepoTable = (props: PageContentProps) => {
   const theme = useTheme();
+  useArrangerTheme(getTableStyles(theme));
   const { NEXT_PUBLIC_ARRANGER_API, NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS } = getConfig();
   const manifestColumns = NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS.split(',')
     .filter((field) => field.trim()) // break it into arrays, and ensure there's no empty field names
