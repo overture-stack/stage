0170fbb980f469ed4c006ad58f2a6da55d021759
Mitchell Shiell <59712867+MitchellShiell@users.noreply.github.com>
2024-04-17 17:28:51 -0400

    compressed images + updated copyright + cleaned tsconfig

--- components/pages/about/HeroComponent.tsx
+++ components/pages/about/HeroComponent.tsx
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
+ * 	Copyright (c) 2024 The Ontario Institute for Cancer Research. All rights reserved
  *
  *  This program and the accompanying materials are made available under the terms of
  *  the GNU Affero General Public License v3.0. You should have received a copy of the
  *  GNU Affero General Public License along with this program.
- *   If not, see <http://www.gnu.org/licenses/>.
+ *  If not, see <http://www.gnu.org/licenses/>.
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -22,8 +22,8 @@
 import { ReactElement } from 'react';
 import { css, useTheme } from '@emotion/react';
 import defaultTheme from '../../theme';
-import overtureOverview from './assets/emblemNoBackground.png';
-import heroDemoBannerWide from './assets/herobannerdemo.png';
+import overtureOverview from './assets/emblemNoBackground.webp';
+import heroDemoBannerWide from './assets/herobannerdemo.webp';
 import HeroLink from '@/components/HeroLink';
 
 type HeroProps = {
--- components/pages/about/PageContent.tsx
+++ components/pages/about/PageContent.tsx
@@ -5,7 +5,7 @@
  *  This program and the accompanying materials are made available under the terms of
  *  the GNU Affero General Public License v3.0. You should have received a copy of the
  *  GNU Affero General Public License along with this program.
- *   If not, see <http://www.gnu.org/licenses/>.
+ *  If not, see <http://www.gnu.org/licenses/>.
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
  *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -25,13 +25,13 @@ import { css, useTheme } from '@emotion/react';
 import defaultTheme from '../../theme';
 import ArticleComponent from './ArticleComponent';
 
-import overtureOverview from './assets/portalOverview.png';
-import retrievalOverview from './assets/dataretrieval.png';
-import submissionOverview from './assets/dataSubmission.png';
-import dataAdmin from './assets/dataAdmin.png';
-import arrangerConfigs from './assets/arrangerConfigs.png';
-import stageUI from './assets/stageUI.png';
-import ego from './assets/ego.png';
+import overtureOverview from './assets/portalOverview.webp';
+import retrievalOverview from './assets/dataretrieval.webp';
+import submissionOverview from './assets/dataSubmission.webp';
+import dataAdmin from './assets/dataAdministration.webp';
+import arrangerConfigs from './assets/arrangerConfigs.webp';
+import stageUI from './assets/stageUI.webp';
+import ego from './assets/ego.webp';
 
 const Content = ({ activeId }: { activeId: string }): ReactElement => {
 	const theme: typeof defaultTheme = useTheme();
