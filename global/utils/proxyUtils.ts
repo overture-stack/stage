2dde65f2fd14f1c81cd43cf832692ee5593ac77f
Leonardo Rivera <leorivera_88@hotmail.com>
2024-05-29 10:20:37 -0400

    verify SSL

--- global/utils/proxyUtils.ts
+++ global/utils/proxyUtils.ts
@@ -6,3 +6,7 @@ const convertToRegexPath = (path: string) => {
 export const removeFromPath = (url: string, path: string) => {
 	return url?.replace(convertToRegexPath(path), '') || '';
 };
+
+// Setting the NODE_TLS_REJECT_UNAUTHORIZED environment variable to '0' makes TLS connections
+// and HTTPS requests insecure by disabling certificate verification
+export const SSLSecured = Number(process.env.NODE_TLS_REJECT_UNAUTHORIZED) != 0;
