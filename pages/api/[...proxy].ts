d394a24c08efff911563b3113e6d73a74610d98f
Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>
2024-05-30 12:54:04 -0400

    add empty line for readability

--- pages/api/[...proxy].ts
+++ pages/api/[...proxy].ts
@@ -47,6 +47,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 		},
 		(err) => {
 			console.error(`Proxy error URL: ${req.url}. Error: ${JSON.stringify(err)}`);
+
 			return res.status(500).json(err);
 		},
 	);
