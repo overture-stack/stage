cf31fcec5808ff7216263006694132a203e1e54b
Saqib Ashraf <sashraf@ML6724-SASHRAF.local>
2025-05-16 12:12:42 -0400

    refactor navbar link buttons such that they are in one component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -28,9 +28,9 @@ import defaultTheme from './theme';
 import { OvertureLogo } from './theme/icons';
 import useAuthContext from '../global/hooks/useAuthContext';
 import { StyledLinkAsButton, InternalLink as Link } from './Link';
-import { EXPLORER_PATH, LOGIN_PATH, USER_PATH } from '../global/utils/constants';
+import { DATA_DICTIONARY_PATH, EXPLORER_PATH, LOGIN_PATH, USER_PATH } from '../global/utils/constants';
 import { getConfig } from '../global/config';
-import LinkToDataDictionary from './LinkToDataDictionary';
+import NavbarLinkButton from './NavbarLinkButton';
 
 const NavBar: React.ComponentType = () => {
 	const { user } = useAuthContext();
@@ -105,40 +105,8 @@ const NavBar: React.ComponentType = () => {
 					align-items: center;
 				`}
 			>
-				<div
-					css={(theme) => css`
-						display: flex;
-						align-items: center;
-						justify-content: center;
-						width: 144px;
-						background-color: ${theme.colors.white};
-						height: 100%;
-						&:hover {
-							background-color: ${theme.colors.grey_2};
-						}
-						border-right: 2px solid ${theme.colors.white};
-					`}
-				>
-					<Link path={EXPLORER_PATH}>
-						<a
-							css={(theme) => css`
-								display: flex;
-								flex: 1;
-								height: 100%;
-								justify-content: center;
-								align-items: center;
-								text-decoration: none;
-								color: ${theme.colors.accent_dark};
-								cursor: pointer;
-								${router.pathname === EXPLORER_PATH ? activeLinkStyle : ''}
-							`}
-						>
-							Data Explorer
-						</a>
-					</Link>
-				</div>
-
-				<LinkToDataDictionary />
+				<NavbarLinkButton path={EXPLORER_PATH} label="Data Explorer" />
+				<NavbarLinkButton path={DATA_DICTIONARY_PATH} label="Data Dictionary" />
 
 				{NEXT_PUBLIC_AUTH_PROVIDER &&
 					(user ? (
