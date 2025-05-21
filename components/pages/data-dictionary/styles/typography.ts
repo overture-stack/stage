c08e4ceec638826813e362925300797b067ca0e4
Saqib Ashraf <sashraf@ML6724-SASHRAF.local>
2025-05-21 16:03:12 -0400

    complete style guide with typography

---
+++ components/pages/data-dictionary/styles/typography.ts
@@ -0,0 +1,162 @@
+import { css } from '@emotion/react';
+
+export const Lato = {
+	Hero: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 40px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Title: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Subtitle: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 30px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Subtitle2: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 20px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Intro_Text: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_bold: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_small: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_bold_small: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Data: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 13px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Data_bold: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 13px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Caption: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 11px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Caption_bold: css`
+		font-family: 'Lato', sans-serif;
+		font-size: 11px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+};
+export const Open_Sans = {
+	Hero: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 40px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Title: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Subtitle: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 30px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Subtitle2: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 20px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Intro_Text: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_bold: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 26px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_small: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Paragraph_bold_small: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 16px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Data: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 13px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Data_bold: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 13px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Caption: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 11px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+	Caption_bold: css`
+		font-family: 'Open Sans', sans-serif;
+		font-size: 11px;
+		font-weight: 400;
+		line-height: 100%;
+	`,
+};
