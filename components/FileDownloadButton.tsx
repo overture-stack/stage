4cb72880e229eae59be4a66a3c6292bdf1c87749
Saqib Ashraf <sashraf@ML6724-SASHRAF.local>
2025-05-29 16:23:34 -0400

    remove accidental commit for the download button that is not yet implemented

--- components/FileDownloadButton.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import FileDownload from './theme/icons/file_download';
-type FileDownloadButtonProps = {};
-
-const FileDownloadButton: FC<FileDownloadButtonProps> = () => {
-	return <FileDownload />;
-};
-export default FileDownloadButton;
