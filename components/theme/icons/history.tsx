411c2d2479d069e740e35383255218a31b8d61e9
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-05-30 09:26:26 -0400

    fix webpack error via switching from stroke-width to strokeWidth

--- components/theme/icons/history.tsx
+++ components/theme/icons/history.tsx
@@ -35,7 +35,7 @@ const History = ({ width, height, style }: IconProps) => {
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
-			stroke-width="2"
+			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		>
