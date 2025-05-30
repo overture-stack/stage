79529496e7679b3f1d1ffec40e552017261810a9
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-05-30 11:50:09 -0400

    initilize the dictionarydownloadbutton component

---
+++ components/DictionaryDownloadButton.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+type DictionaryDownloadButtonProps = {};
+const DictionaryDownloadButton: FC<DictionaryDownloadButtonProps> = () => {
+	return <div></div>;
+};
+
+export default DictionaryDownloadButton;
