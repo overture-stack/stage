7571e59a3a6385e5c91a6dfad8e97f3291949acd
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-06-09 10:10:24 -0400

    fix generated warnings after pr merge

--- components/theme/icons/list_filter.tsx
+++ components/theme/icons/list_filter.tsx
@@ -35,7 +35,7 @@ const ListFilter = ({ style, height, width }: IconProps) => {
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
-			stroke-width="2"
+			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		>
