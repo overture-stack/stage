448ff608efa0e2d1d1049a631dd232999e3f5903
Dan DeMaria <ddemaria@oicr.on.ca>
2025-06-10 17:54:19 -0400

    Merge TableHeader & Table Buttons

--- components/pages/explorer/RepoTable.tsx
+++ components/pages/explorer/RepoTable.tsx
@@ -20,27 +20,12 @@
  */
 
 import { css, useTheme } from '@emotion/react';
-import { CountDisplay, Pagination, Table, Toolbar, useArrangerTheme } from '@overture-stack/arranger-components';
-import { CustomExporterInput } from '@overture-stack/arranger-components/dist/Table/DownloadButton/types';
+import { CountDisplay, Pagination, Table, useArrangerTheme } from '@overture-stack/arranger-components';
 import { UseThemeContextProps } from '@overture-stack/arranger-components/dist/ThemeContext/types';
 import { useMemo } from 'react';
-import urlJoin from 'url-join';
-
-import StyledLink from '@/components/Link';
 import { DMSThemeInterface } from '@/components/theme';
-import { Download } from '@/components/theme/icons';
-import { getConfig } from '@/global/config';
-import { INTERNAL_API_PROXY } from '@/global/utils/constants';
 
-const getTableConfigs = ({
-	apiHost,
-	customExporters,
-	theme,
-}: {
-	apiHost: string;
-	customExporters?: CustomExporterInput;
-	theme: DMSThemeInterface;
-}): UseThemeContextProps => ({
+const getTableConfigs = ({ theme }: { theme: DMSThemeInterface }): UseThemeContextProps => ({
 	callerName: 'RepoTable',
 	components: {
 		Table: {
@@ -58,85 +43,6 @@ const getTableConfigs = ({
 			CountDisplay: {
 				// Table CountDisplay is hidden in order to position CountDisplay with Pagination
 				fontSize: '0px',
-			},
-			DownloadButton: {
-				customExporters,
-				css: css`
-					border-radius: 0.5rem;
-					padding: 0.3rem 0.8rem;
-					:hover {
-						color: ${theme.colors.accent_dark};
-						svg {
-							background-color: ${theme.colors.secondary_light};
-							path {
-								fill: ${theme.colors.accent_dark};
-							}
-						}
-					}
-					:disabled {
-						svg {
-							background-color: ${theme.colors.secondary_light};
-						}
-						path {
-							fill: ${theme.colors.grey_5};
-						}
-					}
-				`,
-				downloadUrl: urlJoin(apiHost, 'download'),
-				label: () => (
-					<>
-						<Download
-							fill={theme.colors.white}
-							style={css`
-								color: ${theme.colors.white};
-								background-color: ${theme.colors.accent_dark};
-								margin-right: 0.2rem;
-							`}
-						/>{' '}
-						Download
-					</>
-				),
-				ListWrapper: {
-					width: '11rem',
-				},
-			},
-			DropDown: {
-				arrowColor: theme.colors.white,
-				arrowTransition: 'all 0s',
-				background: theme.colors.accent_dark,
-				borderColor: theme.colors.grey_5,
-				css: css`
-					${theme.typography.subheading2}
-					border-radius: 0.5rem;
-					line-height: 1.3rem;
-					padding: 0.3rem 0.8rem;
-					:disabled {
-						path {
-							fill: ${theme.colors.grey_5};
-						}
-					}
-					:hover {
-						color: ${theme.colors.accent_dark};
-						svg {
-							path {
-								fill: ${theme.colors.accent_dark};
-							}
-						}
-					}
-				`,
-				disabledFontColor: theme.colors.grey_5,
-				fontColor: theme.colors.white,
-				hoverBackground: theme.colors.secondary_light,
-
-				ListWrapper: {
-					background: theme.colors.white,
-					css: css`
-						${theme.shadow.default}
-					`,
-					fontColor: theme.colors.black,
-					fontSize: '0.7rem',
-					hoverBackground: theme.colors.secondary_light,
-				},
 			},
 			HeaderRow: {
 				borderColor: theme.colors.grey_3,
@@ -170,50 +76,9 @@ const getTableConfigs = ({
 });
 
 const RepoTable = () => {
-	const { NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS } = getConfig();
 	const theme = useTheme();
 
-	const today = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-	const manifestColumns = NEXT_PUBLIC_ARRANGER_MANIFEST_COLUMNS.split(',')
-		.filter((field) => field.trim()) // break it into arrays, and ensure there's no empty field names
-		.map((fieldName) => fieldName.replace(/['"]+/g, '').trim());
-	const customExporters = [
-		{ label: 'File Table', fileName: `data-explorer-table-export.${today}.tsv` }, // exports a TSV with what is displayed on the table (columns selected, etc.)
-		{ label: 'File Manifest', fileName: `score-manifest.${today}.tsv`, columns: manifestColumns }, // exports a TSV with the manifest columns
-		{
-			label: () => (
-				<span
-					css={css`
-						border-top: 1px solid ${theme.colors.grey_3};
-						margin-top: -3px;
-						padding-top: 7px;
-						white-space: pre-line;
-
-						a {
-							margin-left: 3px;
-						}
-					`}
-				>
-					To download files using a file manifest, please follow these
-					<StyledLink
-						css={css`
-							line-height: inherit;
-						`}
-						href="https://www.overture.bio/documentation/guides/download/clientdownload/"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						instructions
-					</StyledLink>
-					.
-				</span>
-			),
-		},
-	];
-
 	const tableConfig = getTableConfigs({
-		apiHost: INTERNAL_API_PROXY.ARRANGER,
-		customExporters,
 		theme,
 	});
 
@@ -222,7 +87,6 @@ const RepoTable = () => {
 	return useMemo(
 		() => (
 			<>
-				<Toolbar />
 				<div
 					css={css`
 						position: relative;
